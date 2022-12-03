13f00dc82d68cbe13b76ff059e0891a43b6c126c
skdev <saeed.karimi@generalinternet.ca>
2022-12-03 10:09:27 -0800

    header navigation done + _document.tsx to fix the styled component issue

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Link from "next/link";
 import styled from "styled-components";
+import { Button } from "@mui/material";
 
 export default function Header() {
 	return (
@@ -11,11 +12,14 @@ export default function Header() {
 			</Logo>
 			<NavLinks>
 				<Link href="/">Home</Link>
+				<Link href="/how">How it works?</Link>
 				<Link href="/about">About</Link>
 				<Link href="/contact">Contact</Link>
-				<Link href="/login">Login</Link>
-				<Link href="/signup">Sign Up</Link>
 			</NavLinks>
+			<UserActions>
+				<LoginButton variant="contained">Login</LoginButton>
+				<SignUpButton  variant="outlined">Sign Up</SignUpButton>
+			</UserActions>
 		</Navbar>
 	);
 }
@@ -23,17 +27,16 @@ export default function Header() {
 // navbar with styled-components
 const Navbar = styled.div`
 	display: grid;
-	grid-template-columns: 60% 40%;
+	grid-template-columns: 30% 53% 17%;
 	width: 100%;
 	padding: 1rem 2rem;
-	background-color: ${(props) => props.theme.colors.cultured};
-	box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+	background-color: ${(props) => props.theme.colors.white};
 `;
 
 // logo with styled-components
 const Logo = styled.div`
-	font-size: 2rem;
-	font-weight: 700;
+	font-size: 2.5rem;
+	font-weight: bold;
 	color: ${(props) => props.theme.colors.celticBlue};
 `;
 
@@ -41,4 +44,35 @@ const NavLinks = styled.div`
 	display: flex;
 	justify-content: space-evenly;
 	align-items: center;
+	margin: 0 15%;
+	color: ${(props) => props.theme.colors.spaceCadet};
+`;
+
+const UserActions = styled.div`
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	justify-content: space-evenly;
+	align-items: center;
+	grid-gap: 2.5rem;
+`;
+
+const LoginButton = styled(Button)`
+	background-color: ${(props) => props.theme.colors.celticBlue};
+	color: ${(props) => props.theme.colors.white};
+	border-radius: 1.5rem;
+	box-shadow: 0;
+	font-family: "Ubuntu", sans-serif;
+	box-shadow: 0;
+	font-size: .85rem;
+	text-transform: none;
+`;
+
+const SignUpButton = styled(Button)`
+	background-color: ${(props) => props.theme.colors.white};
+	color: ${(props) => props.theme.colors.celticBlue};
+	border-radius: 1.5rem;
+	box-shadow: 0;
+	font-family: "Ubuntu", sans-serif;
+	font-size: .85rem;
+	text-transform: none;
 `;
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,10 +1,15 @@
 import Header from './header';
+import styled from 'styled-components';
 
 export default function Layout({ children }) {
 	return (
-		<>
+		<BodyComponent>
 			<Header />
 			<main>{children}</main>
-		</>
+		</BodyComponent>
 	);
 }
+
+const BodyComponent = styled.div`
+	background-color: ${(props) => props.theme.colors.cultured};
+`;
